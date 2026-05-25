4c7b91257ea6877e445550f349db1fb0f7d7bd7e
Gopal Kundu <kundugopal2005@gmail.com>
2026-05-25 23:23:20 +0530

    Bugs fixed

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const baseURL = "https://myowndsatracker-n8wu-qozyuj86k-gopals-projects-e1140103.vercel.app";
-// export const baseURL = http://localhost:5000;
+export const baseURL = "https://myowndsatracker-n8wu.vercel.app";
+// export const baseURL = "http://localhost:5000";
